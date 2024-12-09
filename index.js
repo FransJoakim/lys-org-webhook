dfdc4d9e6f95801a0f2f2da308f7413dc336aa68
Frans Joakim Titulaer <fransjlt@gmail.com>
2024-12-09 23:23:00 +0100

    wip

--- index.js
+++ index.js
@@ -24,6 +24,14 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.get("/", async (req, res) => {
+  try {
+    res.status(200).json({ message: "Hello World" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 app.post("/", async (req, res) => {
   const { data } = req.body;
   const user = data.users[1];
